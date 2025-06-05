c96c878e5e5898cb6b466878e474c865ffce2e85
ono ryo <onopod@gmail.com>
2025-06-05 15:20:05 +0900

    list

--- api/index.py
+++ api/index.py
@@ -1,11 +1,38 @@
-from flask import Flask
+# usage
+# FLASK_APP=api.index.py flask run
+# 参考ページ
+# https://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13101&cb=0.0&ct=9999999&et=9999999&cn=9999999&mb=0&mt=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&fw2=&srch_navi=1
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!!'
+items = [
+    {
+        "id": 1,
+        "title": "ＪＲ山手線 秋葉原駅 15階建 築3年",
+        "price": "143000"
+    }, 
+    {
+        "id": 2,
+        "title": "パークルール大手町",
+        "price": "192000"
+    }
+]
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route("/")
+def index():
+    return "Hello, World!!"
+
+@app.route("/list")
+def list():
+    return render_template("list.html")
+
+@app.route("/list2")
+def list2():
+    return render_template("list2.html", items=items)
+
+@app.route("/detail/<int:id>")
+def detail(id):
+    item = next((item for item in items if item["id"] == id), None)
+    return render_template("detail.html", item=item)
